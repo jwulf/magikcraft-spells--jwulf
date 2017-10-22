83107c2f1be4e24c7a8adc61a439606c9436929f
jwulf <sitapati@worldsankirtan.net>
2017-10-22 16:11:38 +0000

    Update apple.ts

--- apple.ts
+++ apple.ts
@@ -1,8 +1,8 @@
 const magik = magikcraft.io;
 
 function apple(quantity = 1) {
-	 const MATERIAL = Java.type("org.bukkit.Material");
-    const ItemStack = Java.type("org.bukkit.inventory.ItemStack");
+	 const MATERIAL = magik.type("Material");
+    const ItemStack = magik.type("inventory.ItemStack");
     const food = new ItemStack(MATERIAL.APPLE);
 
     for (let i = 0; i ++; i < quantity) {
