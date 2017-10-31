bcbf48c52f37999a11af8c1e980470588c573075
jwulf <sitapati@worldsankirtan.net>
2017-10-31 08:51:20 +0000

    Update interact.ts

---
+++ interact.ts
@@ -0,0 +1,19 @@
+const magik = magikcraft.io;
+
+const CommandCallback = Java.type("io.magikcraft.CommandCallback"),
+    EventPriority = Java.type("org.bukkit.event.EventPriority"),
+    EventCallback = Java.type("io.magikcraft.EventCallback");
+
+function interact() {
+    magik.getPlugin().registerEvent(
+            Java.type("org.bukkit.event.player.PlayerInteractEvent")
+.class,
+        EventPriority.MONITOR,
+        true,
+        new EventCallback({
+            callback: function (event: any) {
+                // Need to do anything?
+                magik.dixit("PlayerInteractEvent!");
+            }
+        }));	
+}
