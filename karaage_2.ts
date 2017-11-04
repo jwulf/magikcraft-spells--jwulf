68aba80415a87d1ff762c3e2e3a4aef5614f668b
jwulf <sitapati@worldsankirtan.net>
2017-11-04 05:03:00 +0000

    Update karaage_2.ts

--- karaage_2.ts
+++ karaage_2.ts
@@ -1,6 +1,6 @@
 var magik = magikcraft.io;
 
-function karaage(n = 1){
+function karaage_2(n = 1){
     const Chicken = magik.type("entity.EntityType").CHICKEN;
     const world = magik.getSender().getWorld();
     const num = Math.min(n, 100);
