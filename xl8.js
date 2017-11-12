c03f9b29609899d7444b9636678ca8e9cecf1632
jwulf <sitapati@worldsankirtan.net>
2017-11-12 08:04:34 +0000

    Update xl8.js

--- xl8.js
+++ xl8.js
@@ -2,6 +2,7 @@ const magik = magikcraft.io;
 
 function xl8() {
 
-    // arguments is an Object 
+    // arguments is an Object with integer keys 
+    // {"0": "first-argument", "1": "second-argument"}
 	magik.dixit(arguments);
 }
