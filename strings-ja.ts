5c63641d56c59e15046cc8882108235f6c6e070e
jwulf <sitapati@worldsankirtan.net>
2017-11-12 11:16:55 +0000

    Update strings-ja.ts

---
+++ strings-ja.ts
@@ -0,0 +1,263 @@
+export const strings = [
+        {
+            name: 'tutorial_welcome',
+            strings:
+            {
+                en: [
+                    'Welcome to',
+                    '&6The Land of Kigam&r',
+                    'in',
+                    '&3The World of&r &4&uMagikcraft'
+                ],
+                ja: [
+                    '&uMagikcraft&r no sekai no kigamu',
+                     'no kuni e yokoso',
+                     '.',
+                     '.'
+                ]
+            }
+        },
+        {
+            name: 'tutorial_magik',
+            strings:
+            {
+                en: [
+                    '--- &4&uMagik&r ---',
+                    'Is a power that allows people',
+                    'to do &6impossible things&r',
+                    'by saying &6special words&r'
+                ],
+                ja: [
+                    '--- &4&uMagikku wa&r ---',
+                    'tokubetsuna kotoba o itte',
+                    'hitobito ga fukanona koto o suru',
+                    'koto o kano ni suru chikaradesu'
+                ]
+            }
+        },
+        {
+            name: 'tutorial_chap_01',
+            strings: {
+                en: [
+                    'Hello, my name is &aM&r.',
+                    '&4&u!!!!Help!!!!!!&r',
+                    '&dSuperMegaCorp&r',
+                    'stole all the &4&umagik',
+                    'in the realm of Kigam.'
+                ],
+                ja: [
+                    "Kon'nichiwa watashinonamaeha M desu",
+                    '&4&u!!!!Tasukete!!!!&r',
+                    '&dSuperMegaCorp&r ga tochi no subete',
+                    'no &4&umaho&r o nusunda'
+                ]
+            }
+        },
+        {
+            name: 'tutorial_chap_02',
+            strings: {
+                en: [
+                    'Brave people joined me',
+                    'and together we beat',
+                    '&dSuperMegaCorp&r.',
+                    'But &4&umagik&r was lost!!!'
+                ],
+                ja: [
+                    "Yukan'na hitobito ga watashi",
+                    'ni kuwawari, issho ni',
+                    '&dSuperMegaCorp&r o utta ga,',
+                     '&4&umaho&r wa ushinawareta'
+                ]
+            }
+        },
+        {
+            name: 'tutorial_chap_03',
+            strings: {
+                en: [
+                    'Now &dSuperMegaCorp&r',
+                    'wants to get the &4&umagik&r back.',
+                    'Find the magik words before they do',
+                    'and bring them to',
+                    'the &a&uAsgard Space Station&r!'
+                ],
+                ja: [
+                    'Ima sugu &dSuperMegaCorp&r',
+                    'wa &4&umajikku&r o torimodosou',
+                    'to shite imasu. Karera ga suru mae',
+                    'ni magik kotoba o mitsukete, sorera',
+                    'o asugarudo uchu suteshon ni motte iki nasai!'
+                ]
+            }
+        },
+        {
+            name: 'tutorial_chap_04',
+            strings: {
+                en: [
+                    '--- &dSuperMegaCorp&r ---',
+                    'Be Warned:',
+                    'All &4&umagik&r is the property of',
+                    '&dSuperMegaCorp&r.',
+                    'Trespassers will be persecuted.'
+                ],
+                ja: [
+                    '--- &dSuperMegaCorp&r ---',
+                    '警告してください！',
+                    'All &4&umagik&r is the property of',
+                    '&dSuperMegaCorp&r.',
+                    '他の人は罰せられる！'
+                ]
+            }
+        },
+        {
+            name: 'tutorial_chap_05',
+            strings: {
+                en: [
+                    'You need to use &4&umagik&r',
+                    'to get out of here',
+                    '&f[|||] &4[|||] &f[|||]',
+                    'Find the magik word'
+                ],
+                ja: [
+                    'あなたは出かけるのに魔法を使う必要があります',
+                    '魔法の言葉を見つける'
+                ]
+            }
+        },
+        {
+            name: 'tutorialwarp',
+            strings: {
+                en: [
+                    '--- &aM&r ---',
+                    'Thank you for who you are',
+                    'and what you are doing',
+                    'for the people of Kigam.',
+                    'Type in &u/warp Lounge',
+                    'it will take you to the next area.',
+                    'Keep doing you, and see you soon!'
+                ]
+            }
+        },
+        {
+            name: 'congrats',
+            strings: {
+                en: [
+                    '&4&uCongratulations!!!&r'
+                ],
+                ja: ['&4&u御目出度う！&r']
+            }
+        },
+        {
+            name: 'arthur',
+            strings: {
+ 
+                en: [
+                    "'''Any sufficiently advanced technology",
+                    'is indistinguishable from magic',
+                    '- Arthur C. Clarke'
+                ]
+            }
+        },
+        {
+            name: 'jungle-instructions',
+            strings: {
+                en: [
+                    'Find the ingredients to make spell with',
+                    '&aThere are five of them...&r',
+                    '&aYou only need three for a spell&r'
+                ]
+            }
+        },
+        {
+            name: 'flight_cast',
+            strings: {
+                en: [
+                    'Cast your remember spell here'
+                ],
+                ja: [
+                    'Oboete iru jumon o koko ni kyasuto suru'
+                ]
+            }
+        },
+        {
+            name: 'flight-goback',
+            strings: {
+                en: [
+                    "Did you find the magik word? It's back, behind you..."
+                ]
+            }
+        },
+        {
+            name: 'flight-end-menu',
+            strings: {
+                en: [
+                    'Did you find the secret passage through the waterfall?',
+                    'To go back to find it: &4&u/warp lounge&r',
+                    'To go on to the next adventure: &4&u/warp flight2&r'
+                ]
+            }
+        },
+        {
+            name: 'flight2-instructions',
+            strings: {
+                en: [
+                    'Fly all the way in one jump!'
+                ]
+            }
+        },
+        {
+            name: 'flight2-end',
+            strings: {
+                en: [
+                    'Congrats!',
+                    'To try again: &4&u/warp flight2&r',
+                    'To go to the next adventure: &4&u/warp jungle&r'
+                ]
+            }
+        },
+        {
+            name: 'kigam-volcano-exit',
+            strings: {
+                en: [
+                    'Stairway'
+                ]
+            }
+        },
+        {
+            name: 'initial_welcome',
+            strings: {
+                en: [
+                    'Welcome to &uMagikcraft',
+                    'Use W,A,S,D to move. Move your mouse to look',
+                    'Once you get that, follow the path'
+                ],
+                ja: [
+                    '&uMagikcraft&rへようこそ！',
+                    'W、A、S、Dを使用して移動します。 見るためにマウスを使用してください。',
+                    '準備が整ったら、パスに従ってください'
+                ]
+            }
+        },
+        {
+            name: 'initial_jumping',
+            strings: {
+                en: [
+                    'Use the Spacebar to jump'
+                ],
+                ja: [
+                    'スペースバーを押してジャンプする'
+                ]
+            }
+        },
+        {
+            name: 'initial_portal',
+            strings: {
+                en: [
+                    '&uMagkicraft&r is connected with &uPortals',
+                    'Walk though, and it will take you to the tutorial level'
+                ],
+                ja: [
+                    'Magikcraftは &uポータル&r',
+                    '歩いてください。 チュートリアルのレベルに進みます。'
+                ]
+            }
+        }
