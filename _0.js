8b1270cd4dd75bc32ac992c348673f41f21ecd2a
jwulf <sitapati@worldsankirtan.net>
2017-11-16 13:12:38 +0000

    Update _0.js

--- _0.js
+++ _0.js
@@ -3,8 +3,6 @@ const magik = magikcraft.io;
 function _0() {
     magik.dixit("yo!")
     var System = Java.type("java.lang.System")
-    magik.dixit("yo!")
-    for each (e in System.env.entrySet()) {
-        magik.dixit(e.key + "=" + e.value)
-    }
+    magik.dixit("yo!");
+    System.env.entrySet().foreach(e => magik.dixit(e.key + "=" + e.value))
 }
