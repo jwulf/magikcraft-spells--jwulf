52278f6f351ebd473345a8d9a83ecab33b6757e1
jwulf <sitapati@worldsankirtan.net>
2017-11-16 13:11:14 +0000

    Update _0.js

--- _0.js
+++ _0.js
@@ -1,10 +1,9 @@
 const magik = magikcraft.io;
 
 function _0() {
-    var sender = magik.getSender();
     var System = Java.type("java.lang.System")
-
+    magik.dixit("yo!")
     for each (e in System.env.entrySet()) {
-        sender.sendMessage(e.key + "=" + e.value)
+        magik.dixit(e.key + "=" + e.value)
     }
 }
