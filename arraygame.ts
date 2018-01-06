4b231187b97c0875f92c60221f55464079f964c8
jwulf <sitapati@worldsankirtan.net>
2018-01-06 13:46:45 +0000

    Update arraygame.ts

--- arraygame.ts
+++ arraygame.ts
@@ -1,20 +1,16 @@
 const magik = magikcraft.io;
 const input = require('input');
 
-const numerals = ['Zero', 'One', 'Two', 'Three',
-    'Four',
-    'Five', 'Six', 'Seven', 'Eight',
-    'Nine'];
+const numerals = ['Zero', 'One', 'Two', 'Three', 'Four','Five', 'Six', 'Seven', 'Eight', 'Nine'];
 
 function arraygame(numeral, player) {
     if (numeral) {
         let n = parseInt(numeral);
         if (n >=0 && n <=9) magik.dixit(numeral + ' is called ' + numerals[n]);
-        if (n != -1) next();
-    } else next();
-    
-}
-
-function next() {
+        if (n == -1) {
+            magik.dixit('Goodbye!');
+        }
+    }
     input(self, 'Pick a number between 0 and 9 inclusive', arraygame);
 }
+
