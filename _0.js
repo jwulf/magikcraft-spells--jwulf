eda859427f35a32d9a6d81bb5a606380fa7c97fb
jwulf <sitapati@worldsankirtan.net>
2018-01-30 00:03:12 +0000

    Update zombie.ts

--- _0.js
+++ _0.js
@@ -1,6 +1,7 @@
 const magik = magikcraft.io;
 
 function _0() {
+    // a couple of tests
     magik.dixit('a'.repeat(3)); 
     magik.dixit('hello'.includes('h'))
     // magik.dixit(global);
--- hw.js
+++
@@ -1,5 +0,0 @@
-const magik = magikcraft.io;
-
-function hw() {
-    magik.dixit("Hello world");
-}
