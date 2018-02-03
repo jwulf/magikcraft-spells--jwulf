0d3d59f2cca6fb0ca3cef3778748f6408fc4499a
jwulf <sitapati@worldsankirtan.net>
2018-02-03 03:34:38 +0000

    Update events.ts

--- events.ts
+++ events.ts
@@ -1,8 +1,7 @@
 const magik = magikcraft.io;
-
-const Emitter = new EventEmitter();
-
 const log = magik.dixit;
+
+let Events = magik.memento.getItem('events');
 
 // const Listener = Java.type("org.bukkit.event");
 // const HandlerList = Java.type("org.bukkit.event.HandlerList");
@@ -10,45 +9,53 @@ const EventPriority = Java.type("org.bukkit.event.EventPriority");
 const EventCallback = Java.type("io.magikcraft.EventCallback");
 
 const eventTypes = {
-	PlayerDeathEvent: 'org.bukkit.event.entity.PlayerDeathEvent',
-	PlayerRespawnEvent: 'org.bukkit.event.player.PlayerRespawnEvent',
-	EntityDamageByEntityEvent: 'org.bukkit.event.entity.EntityDamageByEntityEvent',
-	EntityDamageEvent: 'org.bukkit.event.entity.EntityDamageEvent',
-	ProjectileHitEvent: 'org.bukkit.event.entity.ProjectileHitEvent',
-	PlayerItemConsumeEvent: 'org.bukkit.event.player.PlayerItemConsumeEvent',
-	PlayerQuitEvent: 'org.bukkit.event.player.PlayerQuitEvent',
+    PlayerDeathEvent: 'org.bukkit.event.entity.PlayerDeathEvent',
+    PlayerRespawnEvent: 'org.bukkit.event.player.PlayerRespawnEvent',
+    EntityDamageByEntityEvent: 'org.bukkit.event.entity.EntityDamageByEntityEvent',
+    EntityDamageEvent: 'org.bukkit.event.entity.EntityDamageEvent',
+    ProjectileHitEvent: 'org.bukkit.event.entity.ProjectileHitEvent',
+    PlayerItemConsumeEvent: 'org.bukkit.event.player.PlayerItemConsumeEvent',
+    PlayerQuitEvent: 'org.bukkit.event.player.PlayerQuitEvent',
     PlayerMoveEvent: "org.bukkit.event.player.PlayerMoveEvent",
-	RegionEnterEvent: 'com.mewin.WGRegionEvents.events.RegionEnterEvent',
-	RegionLeaveEvent: 'com.mewin.WGRegionEvents.events.RegionLeaveEvent',
-	PlayerCommandPreprocessEvent: 'org.bukkit.event.player.PlayerCommandPreprocessEvent',
+    RegionEnterEvent: 'com.mewin.WGRegionEvents.events.RegionEnterEvent',
+    RegionLeaveEvent: 'com.mewin.WGRegionEvents.events.RegionLeaveEvent',
+    PlayerCommandPreprocessEvent: 'org.bukkit.event.player.PlayerCommandPreprocessEvent',
 };
 
-const Events = {
 
-	on: (eventName, callback) => Emitter.on(eventName, callback),
+if (!Events) {
+    const Emitter = new EventEmitter();
 
-	unregisterAll: () => {
-		Emitter.removeAllListeners();
-	},
 
-	registerAll: () => {
-		for (let type in eventTypes) {
-			const javaType = eventTypes[type];
-			Emitter.removeAllListeners();
-			magik.getPlugin().registerEvent(
-				Java.type(javaType).class,
-				EventPriority.MONITOR,
-				true,
-				new EventCallback({
-					callback: function (event: any) {						
-						Emitter.emit(type, event);
-					}
-				})
-			);
-		}
-	},
-};
+    const Events = {
+
+        on: (eventName, callback) => Emitter.on(eventName, callback),
+
+        unregisterAll: () => {
+            Emitter.removeAllListeners();
+        },
+
+        registerAll: () => {
+            for (let type in eventTypes) {
+                const javaType = eventTypes[type];
+                Emitter.removeAllListeners();
+                magik.getPlugin().registerEvent(
+                    Java.type(javaType).class,
+                    EventPriority.MONITOR,
+                    true,
+                    new EventCallback({
+                        callback: function (event: any) {
+                            Emitter.emit(type, event);
+                        }
+                    })
+                );
+            }
+        },
+    };
+    magik.memento.setItem('events', Events);
+}
+
 
 function events() {
-	Events.on('PlayerMoveEvent', evt => log(evt.player))
+    Events.on('PlayerMoveEvent', evt => log(evt.player))
 }
