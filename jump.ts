22b4393b1364446ab5d9a85e6ee043431671c412
jwulf <sitapati@worldsankirtan.net>
2018-02-06 01:58:41 +0000

    Update jump.ts

--- jump.ts
+++ jump.ts
@@ -1,8 +1,7 @@
 const magik = magikcraft.io;
 
-magik.exsultus(50)
 function main(power = 100) {
-    magik.dixit(power)
+    magik.dixit(`Papower)
     magik.exsultus(power);
     magik.dixit("Exsultus!");
 }
