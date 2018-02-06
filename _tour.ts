4497c9edf7cec0926a2e17af59c410612d6953a1
jwulf <sitapati@worldsankirtan.net>
2018-02-06 11:32:04 +0000

    Delete _tour.ts

--- _tour.ts
+++
@@ -1,7 +0,0 @@
-const magik = magikcraft.io;
-
-function _tour() {
-	events.forEach((event, i) =>
-        magik.setTimeout(() =>
-            magik.ianuae(magik.toJSON(JSON.parse(event.event).location)), i * 1000));
-}
