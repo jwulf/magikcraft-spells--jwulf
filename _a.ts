d056ca9b23f8e15f35e54ae0fd03cecb2ec4b911
jwulf <josh@magikcraft.io>
2018-09-10 11:02:32 +0000

    Update _a.ts

--- _a.ts
+++ _a.ts
@@ -2,6 +2,6 @@ const magik = magikcraft.io
 
 const e = new EventEmitter();
 
-e.on('something', () => magik.dixit('called'))
+e.on('something', () => magik.dixit('event handler called'))
 
 e.emit('something')
