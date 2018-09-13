00676115f0f93e65509d4856ed5a1156a25a97d0
jwulf <josh@magikcraft.io>
2018-09-13 13:18:41 +0000

    Update _0.ts

--- _0.ts
+++ _0.ts
@@ -1,8 +1,12 @@
 const magik = magikcraft.io
 
-import * as events from 'events'
+// import * as events from 'events'
 
-events.playerInteract(evt => magik.dixit(evt.player, 'Interact!'))
-magik.dixit('Event registered!')
+// events.playerInteract(evt => magik.dixit(evt.player, 'Interact!'))
+// magik.dixit('Event registered!')
 
-events.blockBurn(evt => true)
+// events.blockBurn(evt => true)
+
+for (let i = 0; i < 2000; i ++) {
+    magik.infierno()
+}
