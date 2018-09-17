a7e032f1881ed98bf04cfcdadbcad69eeef47e9e
jwulf <josh@magikcraft.io>
2018-09-17 09:24:23 +0000

    Update _fb.js

--- _fb.js
+++ _fb.js
@@ -1,6 +1,6 @@
 const magik = magikcraft.io;
 
-function main(n=3) {
+function main(n=3000000000000000) {
     magik.doNTimes(()=> magik.infierno(), n, 300);
     magik.dixit('FIREBALLZ YO @ canva!!');
 }
