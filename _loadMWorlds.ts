6ca282ae2bcf3a1bcb253e54518eabadc57eef13
jwulf <josh@magikcraft.io>
2018-09-18 05:39:26 +0000

    Update _loadMWorlds.ts

---
+++ _loadMWorlds.ts
@@ -0,0 +1,9 @@
+const magik = magikcraft.io;
+
+const executeCommand = command => __plugin.server.dispatchCommand(__plugin.server.consoleSender, command)
+
+executeCommand('mv import magikcraft/intro normal')
+executeCommand('mv load magikcraft/intro')
+
+
+
