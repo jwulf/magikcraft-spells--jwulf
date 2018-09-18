59a776605a8bc21238f1c4697b770e53a028c788
jwulf <josh@magikcraft.io>
2018-09-18 09:12:35 +0000

    Update gmall.ts

---
+++ gmall.ts
@@ -0,0 +1,15 @@
+const magik = magikcraft.io;
+const utils = require('utils')
+
+function main(mode = 'a') { 
+    const GameMode = Java.type("org.bukkit.GameMode");
+    const modes = {
+        c: GameMode.CREATIVE,
+        s: GameMode.SURVIVAL,
+        x: GameMode.SPECTATOR
+    }
+    const firstChar = mode.substr(0,1)
+    const newMode = modes[firstChar];
+    utils.players().forEach(p => p.setGameMode(newMode))
+
+}
