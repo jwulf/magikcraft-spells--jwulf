fe7db6563a54d4bffec60041f46f3cd2413cff7b
jwulf <josh@magikcraft.io>
2018-09-18 06:02:03 +0000

    Update _loadMWorlds.ts

--- _loadMWorlds.ts
+++ _loadMWorlds.ts
@@ -2,8 +2,33 @@ const magik = magikcraft.io;
 
 const executeCommand = command => __plugin.server.dispatchCommand(__plugin.server.consoleSender, command)
 
-executeCommand('mv import magikcraft/intro normal')
-executeCommand('mv load magikcraft/intro')
+const worlds = [
+    'magikcraft/intro',
+    'magikcraft/ballerbridge',
+    'magikcraft/kigam-jungletomb',
+    'magikcraft/blockbuster',                  
+    'magikcraft/kigam-outskirts',
+    'magikcraft/egypt-3',
+    'magikcraft/kigam-port',
+    'magikcraft/flight',
+    'magikcraft/kigam-volcano',
+    'magikcraft/flight2',
+    'magikcraft/nordisk',
+    'magikcraft/pirate-island',
+    'magikcraft/jungle',
+    'magikcraft/tutorial',
+    'magikcraft/kigam',
+    'magikcraft/tutorial-backup',
+    'magikcraft/kigam-dungeon',
+    'magikcraft/kigam-frozenvillage',
+    'magikcraft/zombie-arena',
+    'magikcraft/kigam-jail'
+]
+
+worlds.map(w => {
+    executeCommand(`mv import ${w} normal`)
+    executeCommand(`mv load ${w}`)
+})
 
 
 
