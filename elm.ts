a7151eed9129e3a8cdc647cff1899d5194162aa9
jwulf <josh@magikcraft.io>
2018-11-21 11:29:40 +0000

    Update elm.ts

--- elm.ts
+++ elm.ts
@@ -2822,6 +2822,7 @@ const magik = magikcraft.io
 function main() {
     const app = module.exports.Elm.Main.init()
 
+    magik.dixit('Started Elm')
     app.ports.fromElm.subscribe(function(data) {
         magik.dixit(data)
     })
