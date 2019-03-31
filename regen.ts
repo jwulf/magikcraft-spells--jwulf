bb8dc772136ba96b1a794aa0f7579160da75e363
jwulf <josh@magikcraft.io>
2019-03-31 22:25:27 +0000

    Update regen.ts

--- regen.ts
+++ regen.ts
@@ -3,7 +3,7 @@ const magik = magikcraft.io;
 function main() {
     const world = magik.getSender().getWorld()
     const loc = world.getSpawnLocation()
-
+    magik.dixit(loc)
     world.regenerateChunk(loc.chunk.x, loc.chunk.z)
     magik.dixit('Fix it!')
 }
