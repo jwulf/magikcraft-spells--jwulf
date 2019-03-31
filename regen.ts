efde0b78fde4aa6b2bf8d843f8701e29fc1c352c
jwulf <josh@magikcraft.io>
2019-03-31 22:26:24 +0000

    Update regen.ts

--- regen.ts
+++ regen.ts
@@ -7,10 +7,10 @@ function main() {
     world.regenerateChunk(loc.chunk.x, loc.chunk.z)
     magik.dixit('Fix it!')
     const player = magik.getSender()
-const loc: any = player.getLocation()
+const loc1: any = player.getLocation()
 const wolrd: any = player.getWorld()
-const chunkX = loc.chunk.x
-const chunkZ = loc.chunk.z
+const chunkX = loc1.chunk.x
+const chunkZ = loc1.chunk.z
 magik.dixit('chunkX = ' + chunkX) 
 magik.dixit('chunkZ = ' + chunkZ)
 
