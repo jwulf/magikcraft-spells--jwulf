73add21b34437c58814cae5f978a79c804fa0cd2
jwulf <josh@magikcraft.io>
2019-10-07 04:22:36 +0000

    Update villager.ts

--- villager.ts
+++ villager.ts
@@ -1,13 +1,15 @@
 const magik = magikcraft.io;
 
 
-function main() {
+function main(name: string) {
     const Villager = Java.type("org.bukkit.entity.EntityType").VILLAGER;
     const world = magik.getSender().getWorld();
     const start = magik.aspecto();
     const mob = world.spawnEntity(start, Villager);
-    mob.setCustomName('Curly')
-    mob.setCustomNameVisible(true)
+    if (name) {
+        mob.setCustomName(name)
+        mob.setCustomNameVisible(true)
+    }
     const timer = setInterval(() => self.setCompassTarget(mob.getLocation()), 1000)
     setTimeout(() => clearInterval(timer), 60000)
 }
