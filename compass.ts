006555a19726e5bdf36d62b5c731d9e471e98307
jwulf <josh@magikcraft.io>
2019-10-07 06:50:40 +0000

    Update villager.ts

--- compass.ts
+++
@@ -1,8 +0,0 @@
-const magik = magikcraft.io;
-
-function main () {
-    const MATERIAL = Java.type('org.bukkit.Material').COMPASS
-    const ItemStack = Java.type('org.bukkit.inventory.ItemStack')
-    const TheThing = new ItemStack(MATERIAL)
-    self.getInventory().addItem(TheThing)
-}
