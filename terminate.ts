0c2fc3a8fb13c55104ed704e56872a280ab2edc0
jwulf <josh@magikcraft.io>
2019-10-07 07:15:14 +0000

    Update terminate.ts

--- terminate.ts
+++ terminate.ts
@@ -3,6 +3,6 @@ const magik = magikcraft.io;
 function main() {
 	const target: LivingEntity = magik.memento.getItem("target")
     const world = target.getWorld()
-    world.createExplosion(target.location, 1)
+    world.createExplosion(target.location, 2)
     echo(self, `Terminating ${target.getName()}!`)
 }
