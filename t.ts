66aa2cbd64608673d25e58e7fd0c699ac22ea0b5
jwulf <josh@magikcraft.io>
2019-10-07 07:06:01 +0000

    Update t.ts

--- t.ts
+++ t.ts
@@ -1,10 +1,17 @@
 const magik = magikcraft.io;
+const Collectors = java.util.stream.Collectors;
 const track = require('./track').main
 
 function main() {
     const there = magik.aspecto()
     const world = there.world
     const entities = world.getNearbyEntities(there, 2, 2, 2)
+        .stream()
+        .filter(e => e.getName() !== self.name)
+        .collect(Collectors.toList())
+    
+    
+
     if (entities.length > 0) {
         track(entities[0].getName())
     } else {
