f2bafe3dfee0a7602f1cebf50b92a130067ddbff
jwulf <josh@magikcraft.io>
2019-10-13 03:15:42 +0000

    Update bow.ts

--- bow.ts
+++ bow.ts
@@ -1,9 +1,14 @@
 const magik = magikcraft.io;
 
 function main() {
-	const Bow = Java.type('org.bukkit.Material').BOW
     const ItemStack = Java.type('org.bukkit.inventory.ItemStack')
+
+    const Bow = Java.type('org.bukkit.Material').BOW
     const bow = new ItemStack(Bow)
     self.getInventory().addItem(bow)
+
+    const Arrow = Java.type('org.bukkit.Material').ARROW
+    const arrow = new ItemStack(Arrow)
+    self.getInventory().addItem(arrow)
 }
 
