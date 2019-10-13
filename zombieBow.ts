9524e9276e587b0e72b30ea3b92c65d60993a8da
jwulf <josh@magikcraft.io>
2019-10-13 04:20:47 +0000

    Update zombieBow.ts

--- zombieBow.ts
+++ zombieBow.ts
@@ -12,6 +12,7 @@ function main() {
     const bow = new ItemStack(Bow)
     const bowmeta = bow.getItemMeta();
     bowmeta.setDisplayName(ChatColor.GREEN + "Zombie Bow");
+    echo(self, bowmeta.getAttributeModifiers())
     bow.setItemMeta(bowmeta)
     self.getInventory().addItem(bow)
 
