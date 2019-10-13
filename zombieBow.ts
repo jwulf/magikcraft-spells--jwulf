af564c59316ea5d469761a5c4ab51a283e65b977
jwulf <josh@magikcraft.io>
2019-10-13 04:32:00 +0000

    Update zombieBow.ts

--- zombieBow.ts
+++ zombieBow.ts
@@ -2,20 +2,23 @@ const magik = magikcraft.io;
 import * as events from 'events'
 const Collectors = java.util.stream.Collectors;
 const ChatColor = Java.type('org.bukkit.ChatColor')
+const ItemStack = Java.type('org.bukkit.inventory.ItemStack')
+const Material = Java.type('org.bukkit.Material')
 const HANDLER = 'ZombieBowHandler'
 const NAME = ChatColor.GREEN + "Zombie Bow"
+
 function main() {
     const me = self
-    const ItemStack = Java.type('org.bukkit.inventory.ItemStack')
 
-    const Bow = Java.type('org.bukkit.Material').BOW
+    const Bow = Material.BOW
     const bow = new ItemStack(Bow)
     const bowmeta = bow.getItemMeta();
     bowmeta.setDisplayName(NAME);
+    const data = bowmeta.getPersistentDataContainer()
     bow.setItemMeta(bowmeta)
     self.getInventory().addItem(bow)
 
-    const Arrow = Java.type('org.bukkit.Material').ARROW
+    const Arrow = Material.ARROW
     const arrow = new ItemStack(Arrow, 100)
     self.getInventory().addItem(arrow)
     const existingHandler = magik.memento.getItem(HANDLER)
