928107db282c7260016c804702667d8138f8ddea
jwulf <josh@magikcraft.io>
2019-10-13 03:37:23 +0000

    Update bow.ts

--- bow.ts
+++ bow.ts
@@ -1,6 +1,7 @@
 const magik = magikcraft.io;
 import * as events from 'events'
 function main() {
+    const me = self
     const ItemStack = Java.type('org.bukkit.inventory.ItemStack')
 
     const Bow = Java.type('org.bukkit.Material').BOW
@@ -11,6 +12,9 @@ function main() {
     const arrow = new ItemStack(Arrow,100)
     self.getInventory().addItem(arrow)
     events.entityShootBow(e => {
+        if (e.getEntity() != me) {
+            return
+        }
          const aZombie = Java.type("org.bukkit.entity.EntityType").ZOMBIE
          const there = magik.aspecto()
          const world = there.world
